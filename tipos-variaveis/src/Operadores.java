48aee88b633f849e3a105abe9ed98ae9f9c03a56
C2002G <claudiogabriel2002@gmail.com>
2025-04-09 17:12:29 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,40 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // String Comeco = "Meu nome é ";
+        // String NomeCompleto ="Cláudio " + "Gabriel";
+        // String tudo = Comeco + NomeCompleto; // Concatenação de strings
+        // System.out.println(tudo); // Saída: CláudioGabriel
+
+        // int a, b;
+
+        // a = 5;
+        // b = 5;
+
+        // /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        // if(a==b)
+        // resultado = "verdadeiro";
+        // else
+        // resultado = "falso";
+        // */
+
+        // //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        // String resultado = (a!=b) ? "verdadeiro" : "false";
+
+        // System.out.println(resultado);
+
+        // OPERADORES LÓGICOS
+        
+        boolean a = true;
+        boolean b = false;
+
+        if (a && b) {
+            System.out.println("Ambos são verdadeiros.");
+        }
+        if (b || a) {
+            System.out.println("Pelo menos um é verdadeiro.");
+            
+        }
+        System.out.println("fim");
+    
+    }
+}
