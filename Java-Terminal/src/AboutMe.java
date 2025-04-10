45fe02d53e1761b7c406e34422fbda9b819b02cd
C2002G <claudiogabriel2002@gmail.com>
2025-04-10 20:42:45 -0300

    Scanner

--- Java-Terminal/src/AboutMe.java
+++ Java-Terminal/src/AboutMe.java
@@ -1,13 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
         /**
-         * parametros sao os "args" que sao passados como se fosse um input do usuario escrever no cmd
+         * parametros sao os "args" que sao passados como se fosse um input do usuario escrever no cmd ''
          */
+        // mas por ser demorado a questao de digitar no arquivo json para ter as informações, usamos o SCANER, logo abaixo
+                // String nome = args[0];
+                // String sobrenome = args[1];
+                // int idade = Integer.valueOf(args[2]);
+                // double altura = Double.valueOf(args[3]);
 
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.nextLine();
+
+        System.out.println("Dgiite  sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
 
         System.out.println("ola, meu nome é " + nome + " " + sobrenome);
         System.out.println("tenho " + idade + " anos");
